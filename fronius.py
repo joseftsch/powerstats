97b36ea8a020cb4bfac65dee8e434f82419a3569
Josef Tschiggerl <josef@tschiggerl.name>
2021-12-16 10:37:57 +0100

    some corrections and extensions

--- fronius.py
+++ fronius.py
@@ -7,13 +7,18 @@ import mysql.connector
 from configparser import ConfigParser
 from datetime import date
 from influxdb import InfluxDBClient
+import logging
 
 def main():
+    logging.basicConfig(format='%(asctime)s %(module)s %(levelname)s %(funcName)s %(message)s', level=logging.INFO)
+    logging.info("Fronius Power gathering startup!")
     config = ConfigParser()
     try:
-        config.read("/config.ini")
+        config.read("config.ini")
+        if len(config) < 2:
+            raise
     except Exception as e:
-        print("Can not read config file - exit")
+        logging.error("Failed to read config file", exc_info=True)
         sys.exit()
 
     url=config['general']['url']
@@ -22,37 +27,45 @@ def main():
     res={}
     data=GetData(url)
     try:
-        res['inverter_day_energy_wh'] = data['Body']['Data']['DAY_ENERGY']['Values']['1']
-        res['inverter_poc_w'] = data['Body']['Data']['PAC']['Values']['1']
-        res['inverter_total_energy_wh'] = data['Body']['Data']['TOTAL_ENERGY']['Values']['1']
-        res['inverter_year_energy_wh'] = data['Body']['Data']['TOTAL_ENERGY']['Values']['1']
+        res['current_pv_watt'] = int(data['Body']['Data']['Site']['P_PV'])
+        res['energy_pv_today_wh'] = float(data['Body']['Data']['Site']['E_Day'])
+        res['energy_pv_year_wh'] = float(data['Body']['Data']['Site']['E_Year'])
+        res['energy_pv_total_wh'] = float(data['Body']['Data']['Site']['E_Total'])
+        res['autonomy_percent'] = int(data['Body']['Data']['Site']['rel_Autonomy'])
+        res['selfconsumption_percent'] = int(data['Body']['Data']['Site']['rel_SelfConsumption'])
+        res['current_consumption_from_grid_watt'] = float(data['Body']['Data']['Site']['P_Grid'])
+        res['current_consumption_house_watt'] = float(data['Body']['Data']['Site']['P_Load'])
     except Exception as e:
-        print("Unable to assign values ... maybe element missing. Exception: {}".format(str(e)))
+        logging.error("Unable to assign values ... maybe element missing. Exception: {}".format(e), exc_info=True)
         sys.exit()
 
     #verify that we have only integers in dict
     try:
         for k,v in res.items():
-            if not isinstance(v, int):
-                print("{} is not an integer. value: {}".format(str(k),str(v)))
+            if not isinstance(v, int) and not isinstance(v, float):
+                logging.error("{} is not an integer or float. value: {}".format(str(k),str(v)))
                 sys.exit()
     except Exception as e:
-        print("Something went wrong. Exception: {}".format(str(e)))
+        logging.error("Something went wrong. Exception: {}".format(e), exc_info=True)
         sys.exit()
 
     #insert data into mysql
     if 'mysql' in config.sections():
+        logging.info("Insert into MySQL")
         status=MySQLInsert(res,config)
         if not status:
-            print("Something went wrong during MySQL insert")
+            logging.error("Something went wrong during MySQL insert")
             sys.exit()
 
     #insert data into influxdb
     if 'influxdb' in config.sections():
+        logging.info("Insert into InfluxDB")
         status=InfluxDBInsert(res,config)
         if not status:
-            print("Something went wrong during InfluxDB insert")
+            logging.error("Something went wrong during influxdb insert")
             sys.exit()
+
+    logging.info("Fronius Power gathering shutdown!")
 
 def GetData(url:str)->dict:
     try:
@@ -60,7 +73,7 @@ def GetData(url:str)->dict:
         r.raise_for_status()
         return r.json()
     except Exception as e:
-        print("Unable to request data. Exception: {}".format(str(e)))
+        logging.error("Unable to request data. Exception: {}".format(str(e)), exc_info=True)
         sys.exit()
 
 def InfluxDBInsert(res:dict,config:configparser)->bool:
@@ -84,7 +97,7 @@ def InfluxDBInsert(res:dict,config:configparser)->bool:
         client.write_points(datalist)
         client.close()
     except Exception as e:
-        print("Error during InfluxDB connection. Exception: {}".format(str(e)))
+        logging.error("Error during InfluxDB connection. Exception: {}".format(str(e)), exc_info=True)
         return False
     return True
 
@@ -98,7 +111,7 @@ def MySQLInsert(res:dict,config:configparser)->bool:
         mydb = mysql.connector.connect(host=config['mysql']['mysqlhost'],user=config['mysql']['mysqluser'],password=config['mysql']['mysqlpassword'],database=config['mysql']['mysqldb'])
         mycursor = mydb.cursor()
     except mysql.connector.Error as e:
-        print("Error during MySQL connection. Exception: {}".format(str(e)))
+        logging.error("Error during MySQL connection. Exception: {}".format(str(e)), exc_info=True)
         return False
 
     # #insert into db
@@ -107,7 +120,7 @@ def MySQLInsert(res:dict,config:configparser)->bool:
         mydb.commit()
         mydb.close()
     except Exception as e:
-        print("Error during inserting data into MySQL. Exception: {}".format(str(e)))
+        logging.error("Error during insert of data into MySQL. Exception: {}".format(str(e)), exc_info=True)
         return False
 
     return True
